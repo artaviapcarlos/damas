1217d1c53f7a85aa02c80c55289dcf5a4f75ed34
Carlos Artavia <artavia.pcarlos@gmail.com>
2016-10-03 18:46:14 -0600

    node getters & setters

--- Nodo.h
+++ Nodo.h
@@ -15,6 +15,7 @@ protected:
 	Nodo<T>* downLeft;
 	Nodo<T>* downRight;
 	string id;
+
 public:
 	Nodo(T*,Nodo<T> *s = NULL,Nodo<T> *p = NULL);
 	virtual Nodo<T> *getSiguiente()const;
@@ -22,6 +23,14 @@ public:
 	virtual T *getElemento()const;
 	virtual void setSiguiente(Nodo<T>*);
 	virtual void setAnterior(Nodo<T>*);
+	virtual Nodo* getUpLeft() const;
+	virtual Nodo* getUpRight() const;
+	virtual Nodo* getDownLeft() const;
+	virtual Nodo* getDownRight() const;
+	virtual void setUpLeft(Nodo*);
+	virtual void setUpRight(Nodo*);
+	virtual void setDownLeft(Nodo*);
+	virtual void setDownRight(Nodo*);
 	virtual ~Nodo();
 };
 
@@ -46,13 +55,6 @@ void Nodo<T>::setAnterior(Nodo<T>* n){
 }
 
 template <class T>
-void Nodo<T>::setDato(T* a){
-	this->elemento= a;
-
-}
-
-
-template <class T>
  Nodo<T>* Nodo<T>::getSiguiente()const
 {
 	return siguiente;
@@ -69,6 +71,46 @@ template <class T>
 T *Nodo<T>::getElemento()const
 {
 	return elemento;
+}
+
+template <class T>
+Nodo* Nodo::getUpLeft() const {
+	return upLeft;
+}
+
+template <class T>
+void Nodo::setUpLeft(Nodo *upLeft) {
+	this->upLeft = upLeft;
+}
+
+template <class T>
+Nodo* Nodo::getUpRight() const {
+	return upRight;
+}
+
+template <class T>
+void Nodo::setUpRight(Nodo *upRight) {
+	this->upRight = upRight;
+}
+
+template <class T>
+Nodo *Nodo::getDownLeft() const {
+	return downLeft;
+}
+
+template <class T>
+void Nodo::setDownLeft(Nodo *downLeft) {
+	this->downLeft = downLeft;
+}
+
+template <class T>
+Nodo *Nodo::getDownRight() const {
+	return downRight;
+}
+
+template <class T>
+void Nodo::setDownRight(Nodo *downRight) {
+	this->downRight = downRight;
 }
 
 template <class T>
